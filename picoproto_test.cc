7e814b9d8c6a781e9e4baeffd9ca1e266cb4b391
Pete Warden <petewarden@google.com>
2016-11-13 17:52:20 -0800

    Formatted source code

--- picoproto_test.cc
+++ picoproto_test.cc
@@ -1,11 +1,11 @@
 /* Copyright 2016 Pete Warden. All Rights Reserved.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,22 +30,41 @@ inline Dest bit_cast(const Source& source) {
   memcpy(&dest, &source, sizeof(dest));
   return dest;
 }
-  
-#define PP_EXPECT(X) if (!(X)) { PP_LOG(ERROR) << #X \
-<< " expected to be true, but was " << (X) << std::endl; return false; }
 
-#define PP_EXPECT_EQ(X, Y) if ((X) != (Y)) { PP_LOG(ERROR) << #Y \
-  << " expected to be " << #X << ", but was " << (Y) << std::endl; return false; }
+#define PP_EXPECT(X)                                               \
+  if (!(X)) {                                                      \
+    PP_LOG(ERROR) << #X << " expected to be true, but was " << (X) \
+                  << std::endl;                                    \
+    return false;                                                  \
+  }
 
-#define PP_EXPECT_NE(X, Y) if ((X) == (Y)) { PP_LOG(ERROR) << #Y \
-<< " expected not to be " << #X << ", but was" << std::endl; return false; }
+#define PP_EXPECT_EQ(X, Y)                                                 \
+  if ((X) != (Y)) {                                                        \
+    PP_LOG(ERROR) << #Y << " expected to be " << #X << ", but was " << (Y) \
+                  << std::endl;                                            \
+    return false;                                                          \
+  }
 
-#define PP_EXPECT_NEAR(X, Y, E) { double delta = ((X) - (Y)); \
-if (delta < 0) { delta = - delta; } \
-if (delta > (E)) { PP_LOG(ERROR) << #Y \
-<< " expected to be near " << #X << ", but was " << (Y) << std::endl;  \
-return false; } }
-  
+#define PP_EXPECT_NE(X, Y)                                             \
+  if ((X) == (Y)) {                                                    \
+    PP_LOG(ERROR) << #Y << " expected not to be " << #X << ", but was" \
+                  << std::endl;                                        \
+    return false;                                                      \
+  }
+
+#define PP_EXPECT_NEAR(X, Y, E)                                            \
+  {                                                                        \
+    double delta = ((X) - (Y));                                            \
+    if (delta < 0) {                                                       \
+      delta = -delta;                                                      \
+    }                                                                      \
+    if (delta > (E)) {                                                     \
+      PP_LOG(ERROR) << #Y << " expected to be near " << #X << ", but was " \
+                    << (Y) << std::endl;                                   \
+      return false;                                                        \
+    }                                                                      \
+  }
+
 void TestFunction(std::string name, std::function<bool()> test_func) {
   const bool test_result = test_func();
   if (test_result) {
@@ -54,111 +73,112 @@ void TestFunction(std::string name, std::function<bool()> test_func) {
     std::cerr << name << " : FAILED" << std::endl;
   }
 }
-  
+
 void TestAll() {
+  TestFunction("Simple", [] {
+    std::vector<uint8_t> bytes({0x08, 0x96, 0x01});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ(150, message.GetInt(1));
+    return true;
+  });
 
-TestFunction("Simple", []{
-  std::vector<uint8_t> bytes({0x08, 0x96, 0x01});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ(150, message.GetInt(1));
-  return true;
-});
+  TestFunction("String", [] {
+    std::vector<uint8_t> bytes(
+        {0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ("testing", message.GetString(2));
+    return true;
+  });
 
-TestFunction("String", []{
-  std::vector<uint8_t> bytes({0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ("testing", message.GetString(2));
-  return true;
-});
+  TestFunction("StringNoCopy", [] {
+    std::vector<uint8_t> bytes(
+        {0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67});
+    Message message(false);
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ("testing", message.GetString(2));
+    bytes[2] = 'p';
+    PP_EXPECT_EQ("pesting", message.GetString(2));
+    return true;
+  });
 
-TestFunction("StringNoCopy", []{
-  std::vector<uint8_t> bytes({0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67});
-  Message message(false);
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ("testing", message.GetString(2));
-  bytes[2] = 'p';
-  PP_EXPECT_EQ("pesting", message.GetString(2));
-  return true;
-});
+  TestFunction("EmbeddedMessage", [] {
+    std::vector<uint8_t> bytes({0x1a, 0x03, 0x08, 0x96, 0x01});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    Message* embedded_message = message.GetMessage(3);
+    PP_EXPECT_NE(nullptr, embedded_message);
+    PP_EXPECT_EQ(150, embedded_message->GetInt(1));
+    return true;
+  });
 
-TestFunction("EmbeddedMessage", []{
-  std::vector<uint8_t> bytes({0x1a, 0x03, 0x08, 0x96, 0x01});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  Message* embedded_message = message.GetMessage(3);
-  PP_EXPECT_NE(nullptr, embedded_message);
-  PP_EXPECT_EQ(150, embedded_message->GetInt(1));
-  return true;
-});
+  TestFunction("RepeatedVarInt", [] {
+    std::vector<uint8_t> bytes({0x08, 0x96, 0x01, 0x08, 0x7f});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ(150, message.GetInt(1));
+    std::vector<uint64_t> int_array = message.GetUInt64Array(1);
+    PP_EXPECT_EQ(2, int_array.size());
+    PP_EXPECT_EQ(150, int_array[0]);
+    PP_EXPECT_EQ(127, int_array[1]);
+    return true;
+  });
 
-TestFunction("RepeatedVarInt", []{
-  std::vector<uint8_t> bytes({0x08, 0x96, 0x01, 0x08, 0x7f});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ(150, message.GetInt(1));
-  std::vector<uint64_t> int_array = message.GetUInt64Array(1);
-  PP_EXPECT_EQ(2, int_array.size());
-  PP_EXPECT_EQ(150, int_array[0]);
-  PP_EXPECT_EQ(127, int_array[1]);
-  return true;
-});
+  TestFunction("UInt32", [] {
+    std::vector<uint8_t> bytes({0x0d, 0x01, 0x00, 0x00, 0x00});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ(1, message.GetUInt32(1));
+    return true;
+  });
 
-TestFunction("UInt32", []{
-  std::vector<uint8_t> bytes({0x0d, 0x01, 0x00, 0x00, 0x00});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ(1, message.GetUInt32(1));
-  return true;
-});
+  TestFunction("UInt64", [] {
+    std::vector<uint8_t> bytes(
+        {0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_EQ(1, message.GetUInt64(1));
+    return true;
+  });
 
-TestFunction("UInt64", []{
-  std::vector<uint8_t> bytes({0x09, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_EQ(1, message.GetUInt64(1));
-  return true;
-});
+  TestFunction("Float", [] {
+    std::vector<uint8_t> bytes({0x0d});
+    const float value = -42.42f;
+    uint32_t value_as_int = bit_cast<uint32_t>(value);
+    bytes.push_back((value_as_int >> 0) & 0xff);
+    bytes.push_back((value_as_int >> 8) & 0xff);
+    bytes.push_back((value_as_int >> 16) & 0xff);
+    bytes.push_back((value_as_int >> 24) & 0xff);
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_NEAR(-42.42f, message.GetFloat(1), 1e-5f);
+    return true;
+  });
 
-TestFunction("Float", []{
-  std::vector<uint8_t> bytes({0x0d});
-  const float value = -42.42f;
-  uint32_t value_as_int = bit_cast<uint32_t>(value);
-  bytes.push_back((value_as_int >> 0) & 0xff);
-  bytes.push_back((value_as_int >> 8) & 0xff);
-  bytes.push_back((value_as_int >> 16) & 0xff);
-  bytes.push_back((value_as_int >> 24) & 0xff);
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_NEAR(-42.42f, message.GetFloat(1), 1e-5f);
-  return true;
-});
-
-TestFunction("Double", []{
-  std::vector<uint8_t> bytes({0x09});
-  const double value = 23.23;
-  uint64_t value_as_int = bit_cast<uint64_t>(value);
-  bytes.push_back((value_as_int >> 0) & 0xff);
-  bytes.push_back((value_as_int >> 8) & 0xff);
-  bytes.push_back((value_as_int >> 16) & 0xff);
-  bytes.push_back((value_as_int >> 24) & 0xff);
-  bytes.push_back((value_as_int >> 32) & 0xff);
-  bytes.push_back((value_as_int >> 40) & 0xff);
-  bytes.push_back((value_as_int >> 48) & 0xff);
-  bytes.push_back((value_as_int >> 56) & 0xff);
-  Message message;
-  message.ParseFromBytes(bytes.data(), bytes.size());
-  PP_EXPECT_NEAR(23.23f, message.GetDouble(1), 1e-5f);
-  return true;
-});
-  
+  TestFunction("Double", [] {
+    std::vector<uint8_t> bytes({0x09});
+    const double value = 23.23;
+    uint64_t value_as_int = bit_cast<uint64_t>(value);
+    bytes.push_back((value_as_int >> 0) & 0xff);
+    bytes.push_back((value_as_int >> 8) & 0xff);
+    bytes.push_back((value_as_int >> 16) & 0xff);
+    bytes.push_back((value_as_int >> 24) & 0xff);
+    bytes.push_back((value_as_int >> 32) & 0xff);
+    bytes.push_back((value_as_int >> 40) & 0xff);
+    bytes.push_back((value_as_int >> 48) & 0xff);
+    bytes.push_back((value_as_int >> 56) & 0xff);
+    Message message;
+    message.ParseFromBytes(bytes.data(), bytes.size());
+    PP_EXPECT_NEAR(23.23f, message.GetDouble(1), 1e-5f);
+    return true;
+  });
 }
 
 }  // namespace
 }  // namespace picoproto
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
   picoproto::TestAll();
   return 0;
 }
--- tf_to_dot.cc
+++ tf_to_dot.cc
@@ -1,11 +1,11 @@
 /* Copyright 2016 Pete Warden. All Rights Reserved.
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ const int32_t NODE_DEF_NAME_FIELD = 1;
 const int32_t NODE_DEF_OP_FIELD = 2;
 const int32_t NODE_DEF_INPUT_FIELD = 3;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
   if (argc < 2) {
     PP_LOG(ERROR) << "No file name found in arguments" << std::endl;
     return -1;
@@ -38,23 +38,24 @@ int main(int argc, const char * argv[]) {
   fseek(f, 0, SEEK_END);
   long fsize = ftell(f);
   fseek(f, 0, SEEK_SET);
-  
+
   uint8_t* bytes = new uint8_t[fsize];
   fread(bytes, fsize, 1, f);
   fclose(f);
 
   picoproto::Message graph_def;
   graph_def.ParseFromBytes(bytes, fsize);
-  
+
   std::vector<picoproto::Message*> nodes =
-    graph_def.GetMessageArray(GRAPH_DEF_NODE_FIELD);
+      graph_def.GetMessageArray(GRAPH_DEF_NODE_FIELD);
 
   std::cout << "digraph graphname {\n";
   for (picoproto::Message* node : nodes) {
     std::string node_name = node->GetString(NODE_DEF_NAME_FIELD);
     std::string op_name = node->GetString(NODE_DEF_OP_FIELD);
     std::cout << "  \"" << node_name << "\" [label=\"" << op_name << "\"];\n";
-    std::vector<std::string> inputs = node->GetStringArray(NODE_DEF_INPUT_FIELD);
+    std::vector<std::string> inputs =
+        node->GetStringArray(NODE_DEF_INPUT_FIELD);
     for (const std::string& input_name : inputs) {
       size_t colon_pos = input_name.find(":");
       if (colon_pos == std::string::npos) {
@@ -66,12 +67,14 @@ int main(int argc, const char * argv[]) {
       } else {
         hat_pos = 0;
       }
-      std::string input_node_name = input_name.substr(hat_pos, colon_pos - hat_pos);
-      std::cout << "  \"" << input_node_name << "\" -> \"" << node_name << "\";\n";
+      std::string input_node_name =
+          input_name.substr(hat_pos, colon_pos - hat_pos);
+      std::cout << "  \"" << input_node_name << "\" -> \"" << node_name
+                << "\";\n";
     }
   }
   std::cout << "}\n";
-  
+
   delete[] bytes;
   return 0;
 }
